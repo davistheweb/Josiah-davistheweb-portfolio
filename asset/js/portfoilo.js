bbd92b7ac766d71d8f03f0705603e8f0fe4d3736
davistheweb <smartkelvin775@gmail.com>
2024-01-10 00:46:29 +0100

    new changes

--- asset/js/portfoilo.js
+++ asset/js/portfoilo.js
@@ -75,8 +75,8 @@ window.onscroll = () => {
 function scrollTop() {
   const scrollUp = document.getElementById('scroll-up');
   // When the scroll is higher than 400 viewport height, add the show-scroll class to the a tag with the scroll-top class
-  if (this.scrollY >= 400) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
-}
+  if (this.scrollY >= 500) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+};
 window.addEventListener('scroll', scrollTop)
 
 // function: Text Generate
