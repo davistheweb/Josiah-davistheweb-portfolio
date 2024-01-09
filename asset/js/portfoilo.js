5ea0a4b7796f7432f1941d704e9c3a93bf6caefc
davistheweb <smartkelvin775@gmail.com>
2024-01-09 18:48:23 +0100

    new changes

--- asset/js/portfoilo.js
+++ asset/js/portfoilo.js
@@ -71,6 +71,14 @@ window.onscroll = () => {
 	});
 };
 
+/*SHOW SCROLL UP*/
+function scrollTop() {
+  const scrollUp = document.getElementById('scroll-up');
+  // When the scroll is higher than 400 viewport height, add the show-scroll class to the a tag with the scroll-top class
+  if (this.scrollY >= 400) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollTop)
+
 // function: Text Generate
 
 consoleText(['Software Developer','Cyber Security Ethical Hacker', ], 'name',);
